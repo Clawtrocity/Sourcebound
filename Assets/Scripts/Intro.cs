1773ee922ce7960331fc05304a268328cc950a8a
Alex Green <ajgreen414@gmail.com>
2017-08-20 01:26:25 -0400

    v2

--- Assets/Scripts/Intro.cs
+++ Assets/Scripts/Intro.cs
@@ -1,48 +1,22 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Intro : MonoBehaviour {
-    public Animation open;
-    public int counter;
+    public Animator anim;
 
 	// Use this for initialization
 	void Start ()
     {
-        Time.timeScale = 0;
-        Animation();
+        Time.timeScale = 1;
+        StartCoroutine(Timer());
     }
-	
-	// Update is called once per frame
-	void Update ()
+
+    IEnumerator Timer()
     {
-        counter += 1;
-	}
-
-    public void Animation()
-    {
-        if (counter == 200)
-        {
-            open.PlayQueued("Intro1", QueueMode.PlayNow);
-            open.PlayQueued("Outro1", QueueMode.CompleteOthers);
-        }
-
-        if (counter == 600)
-        {
-            open.PlayQueued("Intro2", QueueMode.CompleteOthers);
-            open.PlayQueued("Outro2", QueueMode.CompleteOthers);
-        }
-
-        if (counter == 1300)
-        {
-            open.PlayQueued("Intro3", QueueMode.CompleteOthers);
-            open.PlayQueued("Outro3", QueueMode.CompleteOthers);
-        }
-
-        if (counter == 2000)
-        {
-            open.PlayQueued("Intro4", QueueMode.CompleteOthers);
-            open.PlayQueued("Outro4", QueueMode.CompleteOthers);
-        }
+        yield return new WaitForSeconds(16);
+        SceneManager.LoadScene("Game");
+        SceneManager.UnloadSceneAsync("Intro");
     }
 }
---
+++ Assets/Scripts/Play.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Play : MonoBehaviour {
+
+    public Animator anim;
+
+    public void Start()
+    {
+        Time.timeScale = 0;
+        anim.enabled = false;
+    }
+
+    public void PlayButton()
+    {
+        Time.timeScale = 1;
+        anim.enabled = true;
+        StartCoroutine(Timer());
+        anim.Play("FadeOut");
+    }
+
+    IEnumerator Timer()
+    {
+        yield return new WaitForSeconds(2);
+        SceneManager.LoadScene("Intro");
+        SceneManager.UnloadSceneAsync(SceneManager.GetActiveScene());
+    }
+}
