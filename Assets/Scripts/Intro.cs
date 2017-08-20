cdc0bc615677704dcd4d7843ebb570a4b2f25b2e
Alex Green <ajgreen414@gmail.com>
2017-08-19 20:19:33 -0400

    v1

---
+++ Assets/Scripts/Intro.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Intro : MonoBehaviour {
+    public Animation open;
+    public int counter;
+
+	// Use this for initialization
+	void Start ()
+    {
+        Time.timeScale = 0;
+        Animation();
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        counter += 1;
+	}
+
+    public void Animation()
+    {
+        if (counter == 200)
+        {
+            open.PlayQueued("Intro1", QueueMode.PlayNow);
+            open.PlayQueued("Outro1", QueueMode.CompleteOthers);
+        }
+
+        if (counter == 600)
+        {
+            open.PlayQueued("Intro2", QueueMode.CompleteOthers);
+            open.PlayQueued("Outro2", QueueMode.CompleteOthers);
+        }
+
+        if (counter == 1300)
+        {
+            open.PlayQueued("Intro3", QueueMode.CompleteOthers);
+            open.PlayQueued("Outro3", QueueMode.CompleteOthers);
+        }
+
+        if (counter == 2000)
+        {
+            open.PlayQueued("Intro4", QueueMode.CompleteOthers);
+            open.PlayQueued("Outro4", QueueMode.CompleteOthers);
+        }
+    }
+}
